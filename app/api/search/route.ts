ae6162512803725ae4edf68b31021f38b8d07ace
Kamil <kwolanski3@gmail.com>
2026-04-23 14:45:18 +0200

    feat: add games search route handler using RAWG API

---
+++ app/api/search/route.ts
@@ -0,0 +1,43 @@
+export type SearchGameResult = {
+  rawgId: number;
+  name: string;
+  image: string | null;
+};
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const q = searchParams.get("q");
+
+  const externalApiUrl = new URL("https://api.rawg.io/api/games");
+
+  externalApiUrl.searchParams.set("key", process.env.RAWG_API_KEY || "");
+  externalApiUrl.searchParams.set("page_size", "10");
+
+  if (q) {
+    externalApiUrl.searchParams.set("search", q);
+  }
+
+  try {
+    const response = await fetch(externalApiUrl.toString());
+
+    if (!response.ok) {
+      return Response.json(
+        { error: `Błąd RAWG API: ${response.statusText}` },
+        { status: response.status },
+      );
+    }
+
+    const games = await response.json();
+
+    const results: SearchGameResult[] = games.results.map((game: unknown) => ({
+      rawgId: (game as { id: number }).id,
+      name: (game as { name: string }).name,
+      image: (game as { background_image: string | null }).background_image,
+    }));
+    return Response.json({ results });
+    
+  } catch (error) {
+    console.error("Fetch error:", error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
