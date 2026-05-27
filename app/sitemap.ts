160cb779d4fc6346bfd0e60633d52f650f881a00
Kamil <kwolanski3@gmail.com>
2026-05-27 12:57:46 +0200

    chore: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,5 +8,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
+    {
+      url: "https://gamematcher.app/games-like/gothic",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
   ];
 }
